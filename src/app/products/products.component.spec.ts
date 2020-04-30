861488739ac5a9982d58be77e3a69c2a9c0de6a1
chandrika <chandrikarani@infrrd.ai>
2020-04-30 19:48:44 +0530

    Temporary branch

--- src/app/products/products.component.spec.ts
+++ src/app/products/products.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductsComponent } from './products.component';
+import { By } from '@angular/platform-browser';
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
@@ -22,4 +23,16 @@ describe('ProductsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("does products contain name,price and button",()=>{
+    
+  })
+
+  it("is onclick invoked when click button is clicked",()=>{
+    spyOn(component, 'onClick');
+    let button =  fixture.debugElement.query(By.css('button'));
+    button.triggerEventHandler('click',null);
+    fixture.detectChanges();
+    expect(component.onClick).toHaveBeenCalled();
+})
 });
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
+  products:any[]=[
+    {name:'Nike Shoes',price:8000},
+    {name:'college Bag',price:450},
+    {name:'Phone Case',price:750},
+    {name:'Bluetooth Hearphones',price:2000},
+    {name:'sports Bottle',price:800},
+    {name:'Phone Charger',price:500},
+    {name:'T-Shirt',price:1000},
+    {name:'Dress Material',price:2500},
+    {name:'Hair straightner',price:1500},
+    {name:'Facewash',price:300}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick(){
+    console.log("add button clicked");
   }
 
 }
